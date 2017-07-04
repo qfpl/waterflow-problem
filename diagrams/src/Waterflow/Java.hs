b47a767348a69136b11b4e94829db2b587e128bf
Dave Laing <dave.laing.80@gmail.com>
2017-07-04 16:05:15 +1000

    WIP: more Java tweaking

--- diagrams/src/Waterflow/Java.hs
+++ diagrams/src/Waterflow/Java.hs
@@ -15,6 +15,63 @@ import Graphics.SVGFonts
 
 import Waterflow.Common
 
+txt ::
+  Colour Double ->
+  String ->
+  Diagram B
+txt c s =
+  textSVG_ def s # fc c # lc c
+
+t ::
+  String ->
+  Diagram B
+t = txt gray
+
+tHeights, tLeft, tRight, tLeftMax, tRightMax, tVolume :: Diagram B
+tHeights = txt black "heights"
+tLeft = txt blue "left"
+tRight = txt red "right"
+tLeftMax = txt darkblue "leftMax"
+tRightMax = txt darkred "rightMax"
+tVolume = txt lightblue "volume"
+
+data Ix =
+    Row Int
+  | Segment Int Int
+
+drawCode ::
+  [(Ix, Colour Double)] ->
+  Diagram B
+drawCode ix =
+  let
+    -- TODO break up the segments more
+    segments = [
+        pure $ t "int " ||| tLeft ||| t " = 0;"
+      , pure $ t "int " ||| tRight ||| t " = tHeights.length - 1;"
+      , pure $ t "int " ||| tLeftMax ||| t " = 0;"
+      , pure $ t "int " ||| tRightMax ||| t " = 0;"
+      , pure $ t "int " ||| tVolume ||| t " = 0;"
+      , pure $ t " "
+      , pure $ t "while(" ||| tLeft ||| t " < " ||| tRight ||| t ") {"
+      , pure $ t "  if(" ||| tHeights ||| t "[" ||| tLeft ||| t "] > " ||| tLeftMax ||| t ") {"
+      , pure $ t "    " ||| tLeftMax ||| t " = " ||| tHeights ||| t "[" ||| tLeft ||| t "];"
+      , pure $ t "  }"
+      , pure $ t "  if(" ||| tHeights ||| t "[" ||| tRight ||| t "] > " ||| tRightMax ||| t ") {"
+      , pure $ t "    " ||| tRightMax ||| t " = " ||| tHeights ||| t "[" ||| tRight ||| t "];"
+      , pure $ t "  }"
+      , pure $ t "  if(" ||| tLeftMax ||| t " >= " ||| tRightMax ||| t ") {"
+      , [t "    " , tVolume , t " += ", tRightMax ||| t " - " ||| tHeights ||| t "[" ||| tRight ||| t "]", t ";"]
+      , pure $ t "    " ||| tRight ||| t "--;"
+      , pure $ t "  } else {"
+      , [t "    " , tVolume , t " += " , tLeftMax ||| t " - " ||| tHeights ||| t "[" ||| tLeft ||| t "]", t ";"]
+      , pure $ t "    " ||| tLeft ||| t "--;"
+      , pure $ t "  }"
+      , pure $ t "}"
+      , pure $ t "return " ||| tVolume ||| t ";"
+      ]
+  in
+    vcat . fmap hcat $ segments
+
 data Vars =
   Vars {
     vLeft :: Int
@@ -24,54 +81,24 @@ data Vars =
   , vVolume :: Int
   }
 
-txt ::
-  Colour Double ->
-  String ->
-  Diagram B
-txt c s =
-  textSVG_ def s # fc c # lc c
+initialVars ::
+  Problem ->
+  Vars
+initialVars p =
+  Vars 0 (pred . length . heights $ p) 0 0 0
 
-drawCode ::
-  Maybe Int ->
+annotateProblem ::
+  Problem ->
+  Vars ->
   Diagram B
-drawCode ix =
-  let
-    t = txt gray
-    heights = txt black "heights"
-    left = txt blue "left"
-    right = txt red "right"
-    leftMax = txt darkblue "leftMax"
-    rightMax = txt darkred "rightMax"
-    volume = txt lightblue "volume"
-    lines = [
-        t "int " ||| left ||| t " = 0;"
-      , t "int " ||| right ||| t " = heights.length - 1;"
-      , t "int " ||| leftMax ||| t " = 0;"
-      , t "int " ||| rightMax ||| t " = 0;"
-      , t "int " ||| volume ||| t " = 0;"
-      , t " "
-      , t "while(" ||| left ||| t " < " ||| right ||| t ") {"
-      , t "  if(" ||| heights ||| t "[" ||| left ||| t "] > " ||| leftMax ||| t ") {"
-      , t "    " ||| leftMax ||| t " = " ||| heights ||| t "[" ||| left ||| t "];"
-      , t "  }"
-      , t "  if(" ||| heights ||| t "[" ||| right ||| t "] > " ||| rightMax ||| t ") {"
-      , t "    " ||| rightMax ||| t " = " ||| heights ||| t "[" ||| right ||| t "];"
-      , t "  }"
-      , t "  if(" ||| leftMax ||| t " >= " ||| rightMax ||| t ") {"
-      , t "    " ||| volume ||| t " += " ||| rightMax ||| t " - " ||| heights ||| t "[" ||| right ||| t "];"
-      , t "    " ||| right ||| t "--;"
-      , t "  } else {"
-      , t "    " ||| volume ||| t " += " ||| leftMax ||| t " - " ||| heights ||| t "[" ||| left ||| t "];"
-      , t "    " ||| left ||| t "--;"
-      , t "  }"
-      , t "}"
-      , t "return " ||| volume ||| t ";"
-      ]
-  in
-    vcat lines
+annotateProblem p v =
+  -- TODO still need to draw the variables and the various lines
+  drawProblem p
 
 javaDiagrams ::
   Problem ->
   [Diagram B]
-javaDiagrams _ =
-  [drawCode Nothing]
+javaDiagrams p =
+  -- TODO match the height of these two
+  [ drawCode [] ||| annotateProblem p (initialVars p)
+  ]
