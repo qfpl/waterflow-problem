9d443925ad4f9821e73b17eac01523c3244ffaee
Dave Laing <dave.laing.80@gmail.com>
2017-07-12 09:13:02 +1000

    Some nix changes

--- diagrams/default.nix
+++ diagrams/default.nix
@@ -1,15 +1,26 @@
-{ mkDerivation, base, containers, diagrams, diagrams-lib
-, diagrams-svg, directory, lens, mtl, stdenv, SVGFonts
-}:
-mkDerivation {
-  pname = "waterflow";
-  version = "0.1.0.0";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [
-    base containers diagrams diagrams-lib diagrams-svg directory lens
-    mtl SVGFonts
-  ];
-  license = stdenv.lib.licenses.bsd3;
-}
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  waterflow = haskellPackages.callPackage ./waterflow.nix {};
+
+in
+  pkgs.haskell.lib.overrideCabal waterflow (drv: {
+    executableToolDepends = [pkgs.pandoc];
+    postInstall = ''
+      mkdir -p $out
+      cd $out
+      $out/bin/waterflow
+      pandoc slides.md -s -t slidy -o slides.html
+      rm -Rf $out/bin
+      rm -Rf $out/share
+      rm $out/slides.md
+    '';
+  })
+
